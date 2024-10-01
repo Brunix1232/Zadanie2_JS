7a230129512a15fe644fe1e622e47042e8368204
BRUNIX <Brunus.sowa@outlook.com>
2024-10-01 16:36:37 +0200

    Podpunkt 1

---
+++ script.js
@@ -0,0 +1,12 @@
+const lista1 = document.querySelector('#lista1');
+let tab = [];
+
+for(let i = 0; i < 5; i++) {
+    tab[i] = prompt("Podaj swoje zainteresowania " + (i + 1) + "/5");
+}
+
+for(let i = 0; i < 5; i++) {
+    let newLi = document.createElement("li");
+    newLi.appendChild(document.createTextNode(tab[i]));
+    lista1.appendChild(newLi);
+}
